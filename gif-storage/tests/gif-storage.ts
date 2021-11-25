72f78a85fa02541d3be0ff18d21fb68c6da6f3ad
Maxime Créteur <creteur.maxime@gmail.com>
2021-11-25 19:26:08 +0100

    Add basic gifs counter

--- gif-storage/tests/gif-storage.ts
+++ gif-storage/tests/gif-storage.ts
@@ -1,17 +1,52 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { GifStorage } from '../target/types/gif_storage';
+import { assert } from 'chai';
 
 describe('gif-storage', () => {
+  const { SystemProgram } = anchor.web3;
 
+  console.log("🚀 Starting tests...");
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.GifStorage as Program<GifStorage>;
+  const baseAccount = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('Creates a gif storage', async () => {
+    console.log("Creating a gif storage...")
+    let tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+    console.log("Created !");
+    console.log("📝 Your transaction signature is", tx);
+    assert(tx != null);
+
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    let totalGifs = account.totalGifs;
+    console.log('👀 GIF Count', totalGifs.toString());
+    assert(totalGifs.toNumber() == 0);
   });
+
+  it('Updates gif storage counter', async () => {
+    console.log('Updating total Gifs...');
+   // Call add_gif!
+    await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      },
+    });
+  
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    let totalGifs = account.totalGifs;
+    console.log('👀 GIF Count', totalGifs.toString());
+    assert(totalGifs.toNumber() == 1);
+  });
+
 });
